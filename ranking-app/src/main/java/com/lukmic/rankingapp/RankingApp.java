aa4c8899c95e34a36d1e738d012d3ee9bc7c2f8d
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-04-06 01:20:58 +0200

    Created ranking-app module

---
+++ ranking-app/src/main/java/com/lukmic/rankingapp/RankingApp.java
@@ -0,0 +1,13 @@
+package com.lukmic.rankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RankingApp.class, args);
+    }
+
+}
