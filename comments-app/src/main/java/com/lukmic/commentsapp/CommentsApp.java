3bfe6eb583807d02e1d8862962f1d1fbf68e5ad8
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-04-13 22:47:29 +0200

    Added and configured comments-app module

---
+++ comments-app/src/main/java/com/lukmic/commentsapp/CommentsApp.java
@@ -0,0 +1,13 @@
+package com.lukmic.commentsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentsApp.class, args);
+    }
+
+}
