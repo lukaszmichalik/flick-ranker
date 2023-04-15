7ec5fdc248d5e9986a1d3d615676aca81277908d
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-04-15 16:21:30 +0200

    Fixed null title and release_date when calling tv API

--- ranking-app/src/main/java/com/lukmic/rankingapp/model/Movie.java
+++ ranking-app/src/main/java/com/lukmic/rankingapp/model/Movie.java
@@ -1,12 +1,16 @@
 package com.lukmic.rankingapp.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Movie {
+
+    @JsonAlias("name")
     private String title;
     private String poster_path;
+    @JsonAlias("first_air_date")
     private String release_date;
 }
