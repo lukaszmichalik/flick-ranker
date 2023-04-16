4ae368d0fd6f04bb3ad3353dea720f9527e8dc5c
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-04-16 21:22:29 +0200

    Created AuthorResponse.java
    Created AuthorResponse class to persist author data from response after calling getCommentsByRankingId() controller method of comments-app service

---
+++ ranking-app/src/main/java/com/lukmic/rankingapp/dto/response/AuthorResponse.java
@@ -0,0 +1,12 @@
+package com.lukmic.rankingapp.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthorResponse {
+
+    private Long id;
+    private String username;
+}
