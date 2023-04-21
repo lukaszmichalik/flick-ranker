7bd6a4809d416e725e39b819f1073c0ab460634d
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-04-21 02:08:19 +0200

    Configured Eureka Server

--- ranking-app/src/main/java/com/lukmic/rankingapp/RankingApp.java
+++ ranking-app/src/main/java/com/lukmic/rankingapp/RankingApp.java
@@ -4,11 +4,13 @@ import com.lukmic.rankingapp.configuration.TheMovieDBConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableConfigurationProperties(TheMovieDBConfigProperties.class)
 @EnableFeignClients
+@EnableDiscoveryClient
 public class RankingApp {
 
     public static void main(String[] args) {
