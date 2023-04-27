2f870fe7f5fafc803762875dbf847f47cfc5cbed
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-04-28 01:51:38 +0200

    Initialized vue-app module

---
+++ vue-app/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex p-8 justify-center">
+    <input type="text" class="rounded border-2 border-gray-200 w-72"
+           placeholder="Enter login">
+  </div>
+
+  <div class="flex p-8 justify-center">
+      <input type="text" class="rounded border-2 border-gray-200 w-72"
+      placeholder="Enter password">
+  </div>
+
+  <div class="flex p-8 justify-center">
+    <router-link to="/registration">
+        hej register
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import {computed} from "vue";
+import store from "../store/index.js";
+
+
+</script>
---
+++ vue-app/src/views/Register.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex p-8 justify-center">
+      This is registration page
+  </div>
+</template>
+
+<script setup>
+import {computed} from "vue";
+import store from "../store/index.js";
+
+</script>
