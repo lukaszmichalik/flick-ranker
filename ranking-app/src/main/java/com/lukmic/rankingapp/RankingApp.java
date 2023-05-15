abb62ec5615fccfb71ae786411666229eab21739
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-05-15 21:41:19 +0200

    Added Security to API Gateway

--- ranking-app/src/main/java/com/lukmic/rankingapp/RankingApp.java
+++ ranking-app/src/main/java/com/lukmic/rankingapp/RankingApp.java
@@ -1,6 +1,5 @@
 package com.lukmic.rankingapp;
 
-import com.lukmic.rankingapp.configuration.RsaKeyProperties;
 import com.lukmic.rankingapp.configuration.TheMovieDBConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableConfigurationProperties({TheMovieDBConfigProperties.class, RsaKeyProperties.class})
+@EnableConfigurationProperties(TheMovieDBConfigProperties.class)
 @EnableFeignClients
 public class RankingApp {
 
